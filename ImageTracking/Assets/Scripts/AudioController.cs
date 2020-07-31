a24432c4b1ba4f81086f80621af19d3c28452a4b
Swagat Raj <swagatraj47@gmail.com>
2020-08-01 01:12:30 +0530

    unity image tracking
    EasyAR SDK used as a Vuforia alternative

---
+++ ImageTracking/Assets/Scripts/AudioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip trackingFound;
+
+    [SerializeField]
+    AudioClip trackingLost;
+
+    AudioSource audiosource;
+
+
+    void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    public void PlayonFound()
+    {
+        audiosource.PlayOneShot(trackingFound);
+    }
+    
+    public void PlayonLost()
+    {
+        audiosource.PlayOneShot(trackingLost);
+    }
+
+
+}    
